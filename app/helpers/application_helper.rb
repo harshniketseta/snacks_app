08641733efc9706178f7fefcd2d5a26300dc5d15
Harshniket Seta <harshniketseta@gmail.com>
2015-04-28 00:54:06 +0530

    Change no cache strategy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,8 @@ module ApplicationHelper
   end
 
   def no_cache
-    expires_in 0
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
